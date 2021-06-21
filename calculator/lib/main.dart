44f7a285c5029800cab899996d0ac6729e9d1a4a
Hansi Gayathma <hansigaya99@gmail.com>
2021-06-21 12:45:53 +0530

    Update 5

--- calculator/lib/main.dart
+++ calculator/lib/main.dart
@@ -26,10 +26,77 @@ class SimpleCalculator extends StatefulWidget {
 }
 
 class _SimpleCalculatorState extends State<SimpleCalculator> {
+  get onPressed => null;
+
+  get child => null;
+
   @override
   Widget build(BuildContext context) {
-    return Container(
-        
-    ));
+    return Scaffold(
+      appBar: AppBar(title: Text("Simple Calculator")),
+      body: Column(
+        children: [
+          Container(
+            alignment: Alignment.centerRight,
+            padding: EdgeInsets.fromLTRB(10, 20, 10, 0),
+            child: Text(
+              "0",
+              style: TextStyle(fontSize: 38.0),
+            ),
+          ),
+          Container(
+            alignment: Alignment.centerRight,
+            padding: EdgeInsets.fromLTRB(10, 20, 10, 0),
+            child: Text(
+              "0",
+              style: TextStyle(fontSize: 48.0),
+            ),
+          ),
+          Expanded(
+            child: Divider(),
+          ),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Container(
+                width: MediaQuery.of(context).size.width * .75,
+                child: Table(
+                  children: [
+                    TableRow(
+                      children: [
+                        Container(
+                          height: MediaQuery.of(context).size.height * 0.1,
+                          color: Colors.redAccent,
+                          // ignore: deprecated_member_use
+                          child: FlatButton(
+                            shape: RoundedRectangleBorder(
+                              borderRadius: BorderRadius.circular(0.0),
+                              side: BorderSide(
+                                  color: Colors.white,
+                                  width: 1,
+                                  style: BorderStyle.solid),
+                            ),
+                            padding: EdgeInsets.all(16.0),
+                            onPressed: null,
+                            child: Text(
+                              "C",
+                              style: TextStyle(
+                                fontSize: 30.0,
+                                fontWeight: FontWeight.normal,
+                                color: Colors.white,
+                              ),
+                            ),
+                          ),
+                        ),
+                      ],
+                    ),
+                  ],
+                ),
+              ),
+            ],
+          ),
+        ],
+      ),
+    );
   }
 }
